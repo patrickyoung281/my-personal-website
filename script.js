8afa0c47db545995bdf96cb428f4b6c308d397a2
Patrick Young <patrickyoung281@gmail.com>
2022-04-16 13:51:14 -0400

    Pictures with table

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const btn = document.createElement("button")
+btn.innerHTML = "Click Me"
+document.body.appendChild(btn); 
