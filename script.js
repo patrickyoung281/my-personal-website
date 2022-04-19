43f01699a9c19cab37f6c5292e4ecf896ef919af
Patrick Young <patrickyoung281@gmail.com>
2022-04-19 14:24:08 -0400

    Big Update

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-
 const btn = document.createElement("button")
 btn.innerHTML = "Click Me"
 document.body.appendChild(btn); 
