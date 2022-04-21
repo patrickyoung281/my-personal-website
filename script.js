84c6940d95b4a26db452a4d5f6ef6d4094d84964
Patrick Young <patrickyoung281@gmail.com>
2022-04-21 10:48:36 -0400

    Button Works Now

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const btn = document.createElement("button")
-btn.innerHTML = "Click Me"
-document.body.appendChild(btn); 
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function(e) {
+    const name = prompt("What is your name?");
+
+    document.getElementById("WelcomeMessage").innerHTML="Welcome " + name +"!! :)";
+    document.getElementById("WelcomeMessage").style.color="blue";
+    
+});
